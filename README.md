afbbdac61b80bc444c51d0dff2ae2ce1a4696261
Ubaid-ur-rehman Muhammad <zdac091@live.rhul.ac.uk>
2020-11-03 10:25:26 +0000

    README and code comments added

---
+++ README.md
@@ -0,0 +1,15 @@
+# Application form
+- The user is asked to input their: 
+    - first name
+    - middle name
+    - last name
+    - date of birth
+    - age
+    - house number
+    - first line of address
+    - post code
+    - NI number
+    - the course they're applying to
+    - most recent qualification
+    
+ - the code then displays that information with the appropriate formatting
--- user_details.py
+++ user_details.py
@@ -1,3 +1,4 @@
+# User is asked to input the information being requested. the input value is then stored in a variable of type string or integer depending on the user's input
 first_name = input("Please enter your first name: ")
 middle_name = input("Please enter your middle name: ")
 last_name = input("Please enter your last name: ")
@@ -14,12 +15,19 @@ ni = input("Please enter your NI number: ")
 course = input("Please enter the course you are applying to: ")
 qualification = input("Please enter your most recent qualification: ")
 
+# User info displayed using f-strings for easy concatenation
 print(f"Full name: {first_name.capitalize()} {middle_name.capitalize()} {last_name.capitalize()}")
+# first name, middle name, and last name are each capitalised and concatenated
+
 print(f"Date of birth: {DOB}")
 print(f"Age: {age}")
 print(f"Address: {str(house_number)} {address.capitalize()}")
+# house_number variable will have an integer as value, so it's been casted into a string
+
+# postcode and NI number have all of their characters converted to upper case
 print(f"Postcode: {postcode.upper()}")
 print(f"NI number: {ni.upper()}")
+
 print(f"Course: {course.capitalize()}")
 print(f"Most recent qualification: {qualification.capitalize()}")
 
